add7645eee452ff68e6c56a248c55e0dc3f2c5f4
ghostrib <sh4rkbot@gmail.com>
2018-12-07 15:39:16 -0600

    (feat) Add functioning Signup POST route and add in progress login route.

--- server/app.js
+++ server/app.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const path = require('path');
-const utils = require('./lib/hashUtils');
-const partials = require('express-partials');
-const bodyParser = require('body-parser');
-const Auth = require('./middleware/auth');
-const models = require('./models');
+const express = require("express");
+const path = require("path");
+const utils = require("./lib/hashUtils");
+const partials = require("express-partials");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const Auth = require("./middleware/auth");
+const models = require("./models");
 
 const app = express();
 
-app.set('views', `${__dirname}/views`);
-app.set('view engine', 'ejs');
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "ejs");
 app.use(partials());
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(express.static(path.join(__dirname, "../public")));
 
-
-
-app.get('/', 
-(req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
+app.get("/create", (req, res) => {
+  res.render("index");
 });
 
-app.get('/links', 
-(req, res, next) => {
+app.get("/links", (req, res, next) => {
   models.Links.getAll()
     .then(links => {
       res.status(200).send(links);
@@ -38,15 +39,17 @@ app.get('/links',
     });
 });
 
-app.post('/links', 
-(req, res, next) => {
+app.post("/links", (req, res, next) => {
   var url = req.body.url;
+
   if (!models.Links.isValidUrl(url)) {
     // send back a 404 if link is not valid
     return res.sendStatus(404);
   }
 
-  return models.Links.get({ url })
+  return models.Links.get({
+    url
+  })
     .then(link => {
       if (link) {
         throw link;
@@ -61,7 +64,9 @@ app.post('/links',
       });
     })
     .then(results => {
-      return models.Links.get({ id: results.insertId });
+      return models.Links.get({
+        id: results.insertId
+      });
     })
     .then(link => {
       throw link;
@@ -77,8 +82,28 @@ app.post('/links',
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.post("/login", (req, res, next) => {});
 
+app.post("/signup", (req, res) => {
+  //let username = req.body.username;
+  //let password = req.body.password;
+  const { username, password } = req.body;
 
+  models.Users.create({ username, password })
+    .then(newUserObj => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      // console.log("Error creating new users", error);
+      res.redirect("/signup");
+    });
+
+  // res.send('User created. Message in app.js');
+  //res.end();
+
+  // Pass into users
+  //return models.Users.create(req.body.username)
+});
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
@@ -86,18 +111,22 @@ app.post('/links',
 // If the short-code doesn't exist, send the user to '/'
 /************************************************************/
 
-app.get('/:code', (req, res, next) => {
-
-  return models.Links.get({ code: req.params.code })
+app.get("/:code", (req, res, next) => {
+  return models.Links.get({
+    code: req.params.code
+  })
     .tap(link => {
-
       if (!link) {
-        throw new Error('Link does not exist');
+        throw new Error("Link does not exist");
       }
-      return models.Clicks.create({ linkId: link.id });
+      return models.Clicks.create({
+        linkId: link.id
+      });
     })
     .tap(link => {
-      return models.Links.update(link, { visits: link.visits + 1 });
+      return models.Links.update(link, {
+        visits: link.visits + 1
+      });
     })
     .then(({ url }) => {
       res.redirect(url);
@@ -106,7 +135,7 @@ app.get('/:code', (req, res, next) => {
       res.status(500).send(error);
     })
     .catch(() => {
-      res.redirect('/');
+      res.redirect("/");
     });
 });
 
